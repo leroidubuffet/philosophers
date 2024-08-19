ed29b1e7487e8f7750062fb881561db53f0c2c02
leroidubuffet <mr.bijou@gmail.com>
2024-08-19 13:12:57 +0200

    chore: add mutex demonstration

--- research/thread_and_process.c
+++ research/thread_and_process.c
@@ -4,34 +4,34 @@
 #include <pthread.h>
 
 void *threadFunction(void *arg) {
-    printf("Thread running in the same process. Thread ID = %ld, PID = %d\n", pthread_self(), getpid());
-    return NULL;
+	printf("Thread running in the same process. Thread ID = %ld, PID = %d\n", pthread_self(), getpid());
+	return NULL;
 }
 
 int main(void) {
-    printf("This is the starting process' pid (parent's) = %d\n", getpid());
+	printf("This is the starting process' pid (parent's) = %d\n", getpid());
 
-    pid_t pid;
-    pthread_t tid;
+	pid_t pid;
+	pthread_t tid;
 
-    pid = fork();
+	pid = fork();
 
-    if (pid < 0) {
-        perror("fork failed");
-        return 1;
-    }
-    else if (pid == 0) {  // Child process
-        printf("This is the child process. PID = %d\n", getpid());
-        // Creating a thread in the child process
-        pthread_create(&tid, NULL, threadFunction, NULL);
-        pthread_join(tid, NULL); // Waiting for the thread to finish
-    }
-    else { // Parent process
-        printf("This is the parent process. PID = %d\n", getpid());
-        // Also create a thread in the parent process
-        pthread_create(&tid, NULL, threadFunction, NULL);
-        pthread_join(tid, NULL); // Waiting for the thread to finish
-    }
+	if (pid < 0) {
+		perror("fork failed");
+		return 1;
+	}
+	else if (pid == 0) {  // Child process
+		printf("This is the child process. PID = %d\n", getpid());
+		// Creating a thread in the child process
+		pthread_create(&tid, NULL, threadFunction, NULL);
+		pthread_join(tid, NULL); // Waiting for the thread to finish
+	}
+	else { // Parent process
+		printf("This is the parent process. PID = %d\n", getpid());
+		// Also create a thread in the parent process
+		pthread_create(&tid, NULL, threadFunction, NULL);
+		pthread_join(tid, NULL); // Waiting for the thread to finish
+	}
 
-    return 0;
+	return 0;
 }
