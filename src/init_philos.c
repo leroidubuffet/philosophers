3e5de6b2e577dea2662dcdab55d58182a774b48f
leroidubuffet <mr.bijou@gmail.com>
2024-08-20 12:53:17 +0200

    ref: divide init_philosphers

--- src/init_philos.c
+++ src/init_philos.c
@@ -6,7 +6,7 @@
 /*   By: airyago <airyago@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 10:57:56 by airyago           #+#    #+#             */
-/*   Updated: 2024/08/20 12:22:56 by airyago          ###   ########.fr       */
+/*   Updated: 2024/08/20 12:50:32 by airyago          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,46 +53,68 @@ void *philosopher_routine(void *arg)
 	return NULL;
 }
 
-int init_philosophers(t_philosopher **philosophers, t_fork *forks, t_config *config)
+static int allocate_philosophers(t_philosopher **philosophers, int num_philosophers)
 {
-	int i;
-
-	*philosophers = malloc(sizeof(t_philosopher) * config->num_philosophers);
+	*philosophers = malloc(sizeof(t_philosopher) * num_philosophers);
 	if (!*philosophers)
 	{
 		printf("Failed to allocate memory for philosophers\n");
 		return (1);
 	}
+	return (0);
+}
 
+static int init_philosopher(t_philosopher *philosopher, int id, t_fork *left_fork, t_fork *right_fork, t_config *config)
+{
+	philosopher->id = id;
+	philosopher->left_fork = left_fork;
+	philosopher->right_fork = right_fork;
+	philosopher->config = config;
+	philosopher->last_meal_time = 0; // Initialize the last meal time to zero or the start time
+	philosopher->state_mutex = malloc(sizeof(pthread_mutex_t));
+	if (philosopher->state_mutex == NULL || pthread_mutex_init(philosopher->state_mutex, NULL) != 0)
+	{
+		printf("Failed to initialize state mutex for philosopher %d\n", id);
+		return (1);
+	}
+	if (pthread_create(&philosopher->thread, NULL, philosopher_routine, philosopher) != 0)
+	{
+		printf("Failed to create thread for philosopher %d\n", id);
+		pthread_mutex_destroy(philosopher->state_mutex);
+		free(philosopher->state_mutex);
+		return (1);
+	}
+	return (0);
+}
+
+static void cleanup_philosophers_on_failure(t_philosopher *philosophers, int num_initialized)
+{
+	int	i;
+
+	i = num_initialized - 1;
+	while (i >= 0)
+	{
+		pthread_mutex_destroy(philosophers[i].state_mutex);
+		free(philosophers[i].state_mutex);
+		pthread_cancel(philosophers[i].thread);
+		pthread_join(philosophers[i].thread, NULL);
+		i--;
+	}
+	free(philosophers);
+}
+
+int init_philosophers(t_philosopher **philosophers, t_fork *forks, t_config *config)
+{
+	int i;
+
+	if (allocate_philosophers(philosophers, config->num_philosophers) != 0)
+		return (1);
 	i = 0;
 	while (i < config->num_philosophers)
 	{
-		(*philosophers)[i].id = i + 1;
-		(*philosophers)[i].left_fork = &forks[i];
-		(*philosophers)[i].right_fork = &forks[(i + 1) % config->num_philosophers];
-		(*philosophers)[i].config = config;
-		(*philosophers)[i].last_meal_time = 0; // Initialize the last meal time to zero or the start time
-		(*philosophers)[i].state_mutex = malloc(sizeof(pthread_mutex_t));
-		if ((*philosophers)[i].state_mutex == NULL || pthread_mutex_init((*philosophers)[i].state_mutex, NULL) != 0) {
-			printf("Failed to initialize state mutex for philosopher %d\n", i);
-			// Clean up any created mutexes and free memory
-			while (--i >= 0) {
-				pthread_mutex_destroy((*philosophers)[i].state_mutex);
-				free((*philosophers)[i].state_mutex);
-			}
-			free(*philosophers);
-			return (1);
-		}
-		if (pthread_create(&(*philosophers)[i].thread, NULL, philosopher_routine, &(*philosophers)[i]) != 0) {
-			printf("Failed to create thread for philosopher %d\n", i);
-			// Cleanup already created threads and resources
-			while (--i >= 0) {
-				pthread_mutex_destroy((*philosophers)[i].state_mutex);
-				free((*philosophers)[i].state_mutex);
-				pthread_cancel((*philosophers)[i].thread);
-				pthread_join((*philosophers)[i].thread, NULL);
-			}
-			free(*philosophers);
+		if (init_philosopher(&(*philosophers)[i], i + 1, &forks[i], &forks[(i + 1) % config->num_philosophers], config) != 0)
+		{
+			cleanup_philosophers_on_failure(*philosophers, i);
 			return (1);
 		}
 		i++;
--- src/main.c
+++ src/main.c
@@ -6,14 +6,15 @@
 /*   By: airyago <airyago@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 15:08:18 by airyago           #+#    #+#             */
-/*   Updated: 2024/08/20 12:23:31 by airyago          ###   ########.fr       */
+/*   Updated: 2024/08/20 12:38:28 by airyago          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
 //DEBUG
-void print_config(const t_config *config) {
+#include<inttypes.h>
+static void print_config(const t_config *config) {
     printf("Number of Philosophers: %d\n", config->num_philosophers);
     printf("Time to Die: %d ms\n", config->time_to_die);
     printf("Time to Eat: %d ms\n", config->time_to_eat);
@@ -24,6 +25,25 @@ void print_config(const t_config *config) {
         printf("Times Must Eat: Not specified\n");
     }
 }
+
+static void verify_philosophers(t_philosopher *philosophers, int num_philosophers)
+{
+    int i;
+
+    printf("\nVerifying philosopher initialization:\n");
+    i = 0;
+    while (i < num_philosophers)
+    {
+        printf("Philosopher %d:\n", philosophers[i].id);
+        printf("\tLeft Fork: %p\n", (void*)philosophers[i].left_fork);
+        printf("\tRight Fork: %p\n", (void*)philosophers[i].right_fork);
+        printf("\tState Mutex: %p\n", (void*)philosophers[i].state_mutex);
+        printf("\tThread ID: %" PRIuPTR "\n", (uintptr_t)philosophers[i].thread);
+        printf("\tLast Meal Time: %lld\n", philosophers[i].last_meal_time);
+        i++;
+    }
+}
+
 //DEBUG
 
 int main(int argc, char **argv)
@@ -41,7 +61,8 @@ int main(int argc, char **argv)
 		return (1);
 
 	print_config(&config); //DEBUG
-
+	verify_philosophers(philos, config.num_philosophers); // Verify initialization
+/*
 	// Wait for philosopher threads to finish
 	for (int i = 0; i < config.num_philosophers; i++)
 		pthread_join(philos[i].thread, NULL);
@@ -54,7 +75,7 @@ int main(int argc, char **argv)
 		free(philos[i].state_mutex);
 	}
 	free(philos);
-
+*/
 	return (0);
 }
 
