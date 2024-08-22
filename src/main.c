9943b24006f5bfc61f87322aa243f58c5d08835d
leroidubuffet <mr.bijou@gmail.com>
2024-08-22 17:06:29 +0200

    chore: correct config struct addressing

--- src/main.c
+++ src/main.c
@@ -6,22 +6,22 @@
 /*   By: airyago <airyago@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 13:08:53 by airyago           #+#    #+#             */
-/*   Updated: 2024/08/22 14:44:29 by airyago          ###   ########.fr       */
+/*   Updated: 2024/08/22 17:05:52 by airyago          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	init_config(t_config config, char **argv)
+void	init_config(t_config *config, char **argv)
 {
-	config.num_of_philos = ft_atoi(argv[1]);
-	config.time_to_die = ft_atoi(argv[2]);
-	config.time_to_eat = ft_atoi(argv[3]);
-	config.time_to_sleep = ft_atoi(argv[4]);
+	config->num_of_philos = ft_atoi(argv[1]);
+	config->time_to_die = ft_atoi(argv[2]);
+	config->time_to_eat = ft_atoi(argv[3]);
+	config->time_to_sleep = ft_atoi(argv[4]);
 	if (argv[5])
-		config.num_times_to_eat = ft_atoi(argv[5]);
+		config->num_times_to_eat = ft_atoi(argv[5]);
 	else
-		config.num_times_to_eat = -1;
+		config->num_times_to_eat = -1;
 }
 
 /** philos and forks are initialized in the main function for easier
@@ -36,8 +36,9 @@ int	main(int argc, char **argv)
 
 	if (validate_args(argc, argv) != 0)
 		return (1);
-	init_config(config, argv);
+	init_config(&config, argv);
 	init_program(&program, philos, &config);
 	init_forks(forks, config.num_of_philos);
+	init_threads(&program, forks);
 	return (0);
 }
