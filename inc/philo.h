db7ceb383a2c32c52a01814b4ea8ffa0c299292c
leroidubuffet <mr.bijou@gmail.com>
2024-08-22 13:58:14 +0200

    `chore: Update header file and argument utilities`

--- inc/philo.h
+++ inc/philo.h
@@ -1,18 +1,33 @@
-// File: inc/philo.h
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: airyago <airyago@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/22 13:25:43 by airyago           #+#    #+#             */
+/*   Updated: 2024/08/22 13:38:42 by airyago          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef PHILO_H
-#define PHILO_H
+# define PHILO_H
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
+# include <stdbool.h>
+// 300 philosophers is the maximum number of philosophers in the evaluation
+# define PHILO_MAX 300
 
-// Structures
+// Num utils
+bool	ft_is_digit(const char c);
+int		ft_atoi(const char *str);
 
-typedef struct {
-    int id;
-    // Add any additional fields here
-} Philosopher;
+// Arg utils
+int		validate_args(int argc, char *argv[]);
+void	parse_args(int argc, char **argv, t_config *config);
 
-// Function Declarations
-
-void initialize_philosophers(Philosopher* philosophers, int num_philosophers);
-void cleanup_philosophers(Philosopher* philosophers, int num_philosophers);
 
 #endif // PHILO_H
--- src/main.c
+++ src/main.c
@@ -6,15 +6,20 @@
 /*   By: airyago <airyago@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 13:08:53 by airyago           #+#    #+#             */
-/*   Updated: 2024/08/22 13:09:10 by airyago          ###   ########.fr       */
+/*   Updated: 2024/08/22 13:39:51 by airyago          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	main(int argc, char *argv[])
+int	main(int argc, char **argv)
 {
-    // TODO: Implement the main logic of the program
+	t_program		program;
+	t_philo			philos[PHILO_MAX];
+	pthread_mutex_t	forks[PHILO_MAX];
 
-    return (0);
+	if (validate_args(argc, argv) != 0)
+		return (1);
+
+	return (0);
 }
