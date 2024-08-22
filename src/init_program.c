f8be178b82c179a026222a0fb70d21e597a0fed6
leroidubuffet <mr.bijou@gmail.com>
2024-08-22 14:55:06 +0200

    chore: Initialize program, philos, and forks with provided configuration

--- src/init_program.c
+++ src/init_program.c
@@ -1,7 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_program.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: airyago <airyago@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/22 14:17:29 by airyago           #+#    #+#             */
+/*   Updated: 2024/08/22 14:54:33 by airyago          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
-// Function to initialize the program and its components
-void init_program()
+static void	get_input(t_philo *philo, t_config *config)
 {
-    // TODO: Add initialization code here
+	philo->time_to_die = config->time_to_die;
+	philo->time_to_eat = config->time_to_eat;
+	philo->time_to_sleep = config->time_to_sleep;
+	philo->num_times_to_eat = config->num_times_to_eat;
+}
+
+void	init_philos(t_philo *philos, t_program *program, pthread_mutex_t *forks,
+		char **argv)
+{
+	int	i;
+	int	num_of_philos;
+
+	num_of_philos = program->config->num_of_philos;
+	i = 0;
+	while (i < num_of_philos)
+	{
+		philos[i].id = i + 1;
+		philos[i].eating = 0;
+		philos[i].meals_eaten = 0;
+		get_input(&philos[i], program->config);
+		philos[i].start_time = get_current_time();
+		philos[i].last_meal = get_current_time();
+		philos[i].write_lock = &program->write_lock;
+		philos[i].dead_lock = &program->dead_lock;
+		philos[i].meal_lock = &program->meal_lock;
+		philos[i].dead = &program->dead_flag;
+		philos[i].l_fork = &forks[i];
+		if (i == 0)
+			philos[i].r_fork = &forks[num_of_philos - 1];
+		else
+			philos[i].r_fork = &forks[i - 1];
+		i++;
+	}
+}
+
+void	init_forks(pthread_mutex_t *forks, int philo_num)
+{
+	int	i;
+
+	i = 0;
+	while (i < philo_num)
+	{
+		pthread_mutex_init(&forks[i], NULL);
+		i++;
+	}
+}
+
+void	init_program(t_program *program, t_philo *philos, t_config *config)
+{
+	program->dead_flag = false;
+	program->philos = philos;
+	pthread_mutex_init(&program->write_lock, NULL);
+	pthread_mutex_init(&program->dead_lock, NULL);
+	pthread_mutex_init(&program->meal_lock, NULL);
+	program->config = config;
 }
--- src/main.c
+++ src/main.c
@@ -6,20 +6,38 @@
 /*   By: airyago <airyago@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 13:08:53 by airyago           #+#    #+#             */
-/*   Updated: 2024/08/22 13:39:51 by airyago          ###   ########.fr       */
+/*   Updated: 2024/08/22 14:44:29 by airyago          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void	init_config(t_config config, char **argv)
+{
+	config.num_of_philos = ft_atoi(argv[1]);
+	config.time_to_die = ft_atoi(argv[2]);
+	config.time_to_eat = ft_atoi(argv[3]);
+	config.time_to_sleep = ft_atoi(argv[4]);
+	if (argv[5])
+		config.num_times_to_eat = ft_atoi(argv[5]);
+	else
+		config.num_times_to_eat = -1;
+}
+
+/** philos and forks are initialized in the main function for easier
+ * memory management
+*/
 int	main(int argc, char **argv)
 {
 	t_program		program;
 	t_philo			philos[PHILO_MAX];
 	pthread_mutex_t	forks[PHILO_MAX];
+	t_config		config;
 
 	if (validate_args(argc, argv) != 0)
 		return (1);
-
+	init_config(config, argv);
+	init_program(&program, philos, &config);
+	init_forks(forks, config.num_of_philos);
 	return (0);
 }
