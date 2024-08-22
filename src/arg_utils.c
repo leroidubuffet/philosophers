257079c5dd96dd66ff60ec6e040f8bbb33f51416
leroidubuffet <mr.bijou@gmail.com>
2024-08-22 13:36:37 +0200

    feat: add arg_utils.c

--- src/arg_utils.c
+++ src/arg_utils.c
@@ -1,4 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   arg_utils.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: airyago <airyago@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/17 18:44:34 by airyago           #+#    #+#             */
+/*   Updated: 2024/08/22 13:36:13 by airyago          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
-// Implement your utility functions related to parsing command-line arguments here
-// ...
+static int	is_positive_integer(const char *str)
+{
+	int	i;
+	int	num;
+
+	i = 0;
+	if (str[0] == '\0' || str[0] == '-')
+		return (0);
+	while (str[i])
+	{
+		if (!ft_is_digit(str[i]))
+			return (0);
+		i++;
+	}
+	num = ft_atoi(str);
+	if (num <= 0)
+		return (0);
+	return (1);
+}
+
+/**
+ * Validates the command line arguments.
+ *
+ * @param argc The number of command line arguments.
+ * @param argv An array of strings representing the command line arguments.
+ * @return An integer indicating the success or failure of the validation.
+ */
+int	validate_args(int argc, char **argv)
+{
+	int	i;
+
+	if (argc < 5 || argc > 6)
+	{
+		printf("Usage:\n%s num_philosophers time_to_die "\
+			"time_to_eat time_to_sleep "\
+			"[number_of_times_each_philosopher_must_eat]\n", argv[0]);
+		return (1);
+	}
+	i = 1;
+	while (i < argc)
+	{
+		if (!is_positive_integer(argv[i]))
+		{
+			printf("All arguments must integers bigger than 0." \
+				"'%s' is invalid.\n", argv[i]);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
