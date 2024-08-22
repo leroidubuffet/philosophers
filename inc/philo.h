9ac4144e78c230a7d3125ae8e05fb2664f6f768d
leroidubuffet <mr.bijou@gmail.com>
2024-08-22 14:18:22 +0200

    chore: rearrange struct members order and change var types for memory optimization

--- inc/philo.h
+++ inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: airyago <airyago@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 13:25:43 by airyago           #+#    #+#             */
-/*   Updated: 2024/08/22 13:38:42 by airyago          ###   ########.fr       */
+/*   Updated: 2024/08/22 14:16:48 by airyago          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,41 @@
 // 300 philosophers is the maximum number of philosophers in the evaluation
 # define PHILO_MAX 300
 
+typedef struct s_config
+{
+	int	num_of_philos;
+	int	num_times_to_eat;
+}	t_config;
+
+typedef struct s_philo
+{
+	pthread_t		thread;
+	pthread_mutex_t	*r_fork;
+	pthread_mutex_t	*l_fork;
+	pthread_mutex_t	*write_lock;
+	pthread_mutex_t	*dead_lock;
+	pthread_mutex_t	*meal_lock;
+	size_t			last_meal;
+	size_t			time_to_die;
+	size_t			time_to_eat;
+	size_t			time_to_sleep;
+	size_t			start_time;
+	int				meals_eaten;
+	bool			eating;
+	bool			*dead;
+	char			id;
+}					t_philo;
+
+typedef struct s_program
+{
+	t_config		*config;
+	t_philo			*philos;
+	pthread_mutex_t	dead_lock;
+	pthread_mutex_t	meal_lock;
+	pthread_mutex_t	write_lock;
+	bool			dead_flag;
+}					t_program;
+
 // Num utils
 bool	ft_is_digit(const char c);
 int		ft_atoi(const char *str);
@@ -28,6 +63,5 @@ int		ft_atoi(const char *str);
 // Arg utils
 int		validate_args(int argc, char *argv[]);
 void	parse_args(int argc, char **argv, t_config *config);
-
 
 #endif // PHILO_H
