70989eaf0c44e25462b580d7260e8fb21d30fbc4
leroidubuffet <mr.bijou@gmail.com>
2024-08-22 17:12:36 +0200

    feat: print_message function

---
+++ src/print_utils.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: airyago <airyago@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/22 17:09:11 by airyago           #+#    #+#             */
+/*   Updated: 2024/08/22 17:12:00 by airyago          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	print_message(char *str, t_philo *philo)
+{
+	size_t	time;
+
+	pthread_mutex_lock(philo->write_lock);
+	time = get_current_time() - philo->start_time;
+	if (!dead_loop(philo))
+		printf("%zu %d %s\n", time, philo->id, str);
+	pthread_mutex_unlock(philo->write_lock);
+}
