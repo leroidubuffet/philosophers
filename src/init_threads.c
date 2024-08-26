ccb4235c910a3921bf50ad8b2ec50cd284c41ea5
leroidubuffet <mr.bijou@gmail.com>
2024-08-26 13:24:56 +0200

    ref: recover check_philo_died function and update it in philo_lifecylce

--- src/init_threads.c
+++ src/init_threads.c
@@ -12,17 +12,17 @@
 
 #include "philo.h"
 // Previously check_philo_dead
-// int	is_philo_dead(t_philo *philo)
-// {
-// 	pthread_mutex_lock(philo->dead_lock);
-// 	if (*philo->dead == 1)
-// 	{
-// 		pthread_mutex_unlock(philo->dead_lock);
-// 		return (1);
-// 	}
-// 	pthread_mutex_unlock(philo->dead_lock);
-// 	return (0);
-// }
+static int	check_philo_died(t_philo *philo)
+{
+	pthread_mutex_lock(philo->dead_lock);
+	if (*philo->dead == true)
+	{
+		pthread_mutex_unlock(philo->dead_lock);
+		return (1);
+	}
+	pthread_mutex_unlock(philo->dead_lock);
+	return (0);
+}
 
 // Wait 1 millisecond to avoid deadlock
 static void	*philo_lifecycle(void *philo_ptr)
@@ -32,7 +32,7 @@ static void	*philo_lifecycle(void *philo_ptr)
 	philo = (t_philo *)philo_ptr;
 	if (philo->id % 2 == 0)
 		ft_sleep(1);
-	while (!is_philo_dead(philo))
+	while (!check_philo_died(philo))
 	{
 		philo_eat(philo);
 		philo_sleep(philo);
@@ -44,7 +44,7 @@ static void	*philo_lifecycle(void *philo_ptr)
 int	init_threads(t_program *program, pthread_mutex_t *forks)
 {
 	pthread_t	thread_monitor;
-	size_t			i;
+	size_t		i;
 
 	if (pthread_create(&thread_monitor, NULL, &monitor_philos, program) != 0)
 		cleanup_resources("Thread creation error", program, forks);
