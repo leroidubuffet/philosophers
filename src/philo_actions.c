eca2044381f5ad1556bde705e1210731607e51e7
leroidubuffet <mr.bijou@gmail.com>
2024-08-27 11:18:55 +0200

    chore: divide philo actions and norminette cleanup

--- src/philo_actions.c
+++ src/philo_actions.c
@@ -22,34 +22,47 @@ void	philo_think(t_philo *philo)
 void	philo_sleep(t_philo *philo)
 {
 	log_philo_status("is sleeping", philo);
-	ft_sleep(*philo->time_to_sleep);
+	ft_sleep(philo->config->time_to_sleep);
 }
 
 //** If there is only one philosopher, we unlock the fork and
 //** wait until the philosopher dies */
-void	philo_eat(t_philo *philo)
+static void	update_meal_info(t_philo *philo)
 {
-	if (is_philo_dead(philo))
-		return ;
+	pthread_mutex_lock(&philo->program->meal_lock);
+	philo->last_meal = get_current_time();
+	philo->meals_eaten++;
+	pthread_mutex_unlock(&philo->program->meal_lock);
+}
+
+static int	take_forks(t_philo *philo)
+{
 	if (pthread_mutex_lock(philo->r_fork) != 0)
 	{
 		log_philo_status("error: unable to lock right fork", philo);
-		return ;
+		return (0);
 	}
 	log_philo_status("has taken a fork", philo);
 	if (philo->config->num_of_philos == 1)
 	{
-		ft_sleep(*philo->time_to_die);
+		ft_sleep(philo->config->time_to_die);
 		pthread_mutex_unlock(philo->r_fork);
-		return ;
+		return (0);
 	}
 	if (pthread_mutex_lock(philo->l_fork) != 0)
 	{
 		pthread_mutex_unlock(philo->r_fork);
 		log_philo_status("error: unable to lock left fork", philo);
-		return ;
+		return (0);
 	}
 	log_philo_status("has taken a fork", philo);
+	return (1);
+}
+
+void	philo_eat(t_philo *philo)
+{
+	if (is_philo_dead(philo) || !take_forks(philo))
+		return ;
 	if (is_philo_dead(philo))
 	{
 		pthread_mutex_unlock(philo->r_fork);
@@ -58,11 +71,8 @@ void	philo_eat(t_philo *philo)
 	}
 	philo->eating = true;
 	log_philo_status("is eating", philo);
-	pthread_mutex_lock(philo->meal_lock);
-	philo->last_meal = get_current_time();
-	philo->meals_eaten++;
-	pthread_mutex_unlock(philo->meal_lock);
-	ft_sleep(*philo->time_to_eat);
+	update_meal_info(philo);
+	ft_sleep(philo->config->time_to_eat);
 	philo->eating = false;
 	pthread_mutex_unlock(philo->l_fork);
 	pthread_mutex_unlock(philo->r_fork);
