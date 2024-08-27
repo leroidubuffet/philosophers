228bec31ce7be762f58505cdac626d018441c15e
leroidubuffet <mr.bijou@gmail.com>
2024-08-27 10:58:57 +0200

    chore: fix structs, reorder function signatures

--- inc/philo.h
+++ inc/philo.h
@@ -36,22 +36,16 @@ typedef struct s_config
 typedef struct s_philo
 {
 	pthread_t		thread;
+	t_program		*program;
 	pthread_mutex_t	*r_fork;
 	pthread_mutex_t	*l_fork;
-	pthread_mutex_t	*write_lock;
-	pthread_mutex_t	*dead_lock;
-	pthread_mutex_t	*meal_lock;
-	size_t			*time_to_die;
-	size_t			*time_to_eat;
-	size_t			*time_to_sleep;
-	int				*num_times_to_eat;
+	t_config		*config;
 	size_t			last_meal;
 	size_t			start_time;
+	int				id;
 	int				meals_eaten;
 	bool			eating;
 	bool			*dead;
-	int			id;
-	t_config		*config;
 }					t_philo;
 
 typedef struct s_program
@@ -64,28 +58,12 @@ typedef struct s_program
 	bool			dead_flag;
 }					t_program;
 
-// Num utils
-bool	ft_is_digit(const char c);
-int		ft_atoi(const char *str);
-
-// Arg utils
-int		validate_args(int argc, char *argv[]);
-void	parse_args(int argc, char **argv, t_config *config);
-
-// Program initialization
+// Initialization
 void	init_program(t_program *program, t_philo *philos, t_config *config);
 void	init_forks(pthread_mutex_t *forks, int philo_num);
-void	init_philos(t_philo *philos, t_program *program, pthread_mutex_t *forks);
+void	init_philos(t_philo *philos, t_program *program,
+			pthread_mutex_t *forks);
 int		init_threads(t_program *program, pthread_mutex_t *forks);
-
-// Utils
-int		ft_sleep(size_t milliseconds);
-void	cleanup_resources(char *str, t_program *program, pthread_mutex_t *forks);
-int		ft_strlen(const char *str);
-
-// Simulation control
-void	*monitor_philos(void *program_ptr);
-int		is_philo_dead(t_philo *philo);
 
 // Philosopher actions
 void	*philosopher_routine(void *arg);
@@ -93,8 +71,24 @@ void	philo_eat(t_philo *philo);
 void	philo_sleep(t_philo *philo);
 void	philo_think(t_philo *philo);
 
+// Simulation control
+void	*monitor_philos(void *program_ptr);
+int		is_philo_dead(t_philo *philo);
+
 // Utility functions
 size_t	get_current_time(void);
 void	log_philo_status(char *str, t_philo *philo);
+int		ft_sleep(size_t milliseconds);
+void	cleanup_resources(char *str, t_program *program,
+			pthread_mutex_t *forks);
+int		ft_strlen(const char *str);
+
+// Argument handling
+int		validate_args(int argc, char *argv[]);
+void	parse_args(int argc, char **argv, t_config *config);
+
+// Number utilities
+bool	ft_is_digit(const char c);
+int		ft_atoi(const char *str);
 
 #endif // PHILO_H
