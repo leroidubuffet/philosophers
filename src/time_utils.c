ca9c0c2d19c53ffc10475de4ff08b4ee087bb414
Yago Bolivar barbudo <ybolivar@c1r17s6.42madrid.com>
2024-08-29 11:18:40 +0200

    chore: add comments and fix norminette

--- src/time_utils.c
+++ src/time_utils.c
@@ -3,15 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   time_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: airyago <airyago@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ybolivar <ybolivar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 13:43:32 by airyago           #+#    #+#             */
-/*   Updated: 2024/08/22 16:20:34 by airyago          ###   ########.fr       */
+/*   Updated: 2024/08/29 11:18:28 by ybolivar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+/**
+ * Retrieves the current time.
+ * timeval is used to obtain the current time with microsecond precision
+ * tv_sec part represents the number of seconds since the Epoch
+ * tv_usec part represents the number of microseconds
+ * @return The current time as a size_t value.
+ */
 size_t	get_current_time(void)
 {
 	struct timeval	time;
@@ -24,6 +31,12 @@ size_t	get_current_time(void)
 	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
 }
 
+/**
+ * Suspends the execution of the current thread for 
+ * the specified number of milliseconds.
+ *
+ * @param milliseconds The number of milliseconds to sleep.
+ */
 int	ft_sleep(size_t milliseconds)
 {
 	size_t			start;
